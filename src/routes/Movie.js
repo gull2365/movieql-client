0f5b327ef730627297f6dfe1391afb28502903f2
Gull 2365 <jych2365@gmail.com>
2023-08-10 10:37:18 +0900

    feat: writeFragment

--- src/routes/Movie.js
+++ src/routes/Movie.js
@@ -55,18 +55,36 @@ const GET_MOVIE = gql`
 
 export default function Movie() {
   const { id } = useParams();
-  const { data, loading } = useQuery(GET_MOVIE, {
+  const {
+    data,
+    loading,
+    client: { cache },
+  } = useQuery(GET_MOVIE, {
     variables: {
       movieId: id,
     },
   });
-
+  const onClick = () => {
+    cache.writeFragment({
+      id: `Movie:${id}`,
+      fragment: gql`
+        fragment MovieFragment on Movie {
+          isLiked
+        }
+      `,
+      data: {
+        isLiked: !data.movie.isLiked,
+      },
+    });
+  };
   return (
     <Container>
       <Column>
         <Title>{loading ? "Loading..." : `${data.movie?.title}`}</Title>
         <Subtitle>⭐️ {data?.movie?.rating}</Subtitle>
-        <button>{data?.movie?.isLiked ? " unlike" : "Like"}</button>
+        <button onClick={onClick}>
+          {data?.movie?.isLiked ? " unlike" : "Like"}
+        </button>
       </Column>
       <Image bg={data?.movie?.medium_cover_image} />
     </Container>
